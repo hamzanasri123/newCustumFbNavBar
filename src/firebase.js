a49b3140bc5a4dd1fec31103ceb3b363047bbe8a
hamza nasri <hamzanasri16@gmail.com>
2020-12-24 19:23:07 +0100

    NavBarFacebookByHamza

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqe4UTu7SLFtjg2eDi0L8SrbYEv4GE3Ro",
+  authDomain: "facebook-clone-b11b4.firebaseapp.com",
+  projectId: "facebook-clone-b11b4",
+  storageBucket: "facebook-clone-b11b4.appspot.com",
+  messagingSenderId: "459905519894",
+  appId: "1:459905519894:web:f0daf024fd6f3ed5944037",
+  measurementId: "G-C0HF65EPXZ"
+};
